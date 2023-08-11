285eca9df3b0b2ca4bc829ca0a4f7f1c4f400c55
kuku67011 <hyzeala28@gmail.com>
2023-08-11 15:21:06 +0100

    styles.css, index.html,index.js,slider.js

---
+++ JS/index.js
@@ -0,0 +1,11 @@
+const openNav = document.querySelector(".open-btn");
+const closeNav = document.querySelector(".close-btn");
+const menu = document.querySelector(".nav-list");
+
+openNav.addEventListener("click", () => {
+  menu.classList.add("show");
+});
+
+closeNav.addEventListener("click", () => {
+  menu.classList.remove("show");
+});
