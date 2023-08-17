dea87c07fa885b9e660f67c330a50e4f2821fe99
kuku67011 <hyzeala28@gmail.com>
2023-08-17 03:34:34 +0100

    payment methods

--- JS/slider.js
+++ JS/slider.js
@@ -1,4 +1,4 @@
-const swiper1 = new Swiper('.slider-1',{
+const swiper1 = new Swiper(".slider-1",{
    autoplay:{
     delay:3500,
     disableOnInteraction:false
@@ -33,22 +33,22 @@ const swiper1 = new Swiper('.slider-1',{
     },
   });
   
-//   const swiper3 = new Swiper(".slider-3", {
-//     loop: true,
-//     grabCursor: true,
-//     autoplay: {
-//       delay: 3500,
-//       disableOnInteraction: false,
-//     },
-//     spaceBetween: 30,
-//     slidesPerView: 2,
-//     breakpoints: {
-//       768: {
-//         slidesPerView: 3,
-//       },
-//       1024: {
-//         slidesPerView: 5,
-//       },
-//     },
-//   });
+  const swiper3 = new Swiper(".slider-3", {
+    loop: true,
+    grabCursor: true,
+    autoplay: {
+      delay: 3500,
+      disableOnInteraction: false,
+    },
+    spaceBetween: 30,
+    slidesPerView: 2,
+    breakpoints: {
+      768: {
+        slidesPerView: 3,
+      },
+      1024: {
+        slidesPerView: 5,
+      },
+    },
+  });
   
