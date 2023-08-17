c9f2ffdfa1b5075446832cb014277c30e0408c2c
kuku67011 <hyzeala28@gmail.com>
2023-08-17 17:53:04 +0100

    added images and scroll effect

--- JS/index.js
+++ JS/index.js
@@ -9,3 +9,15 @@ openNav.addEventListener("click", () => {
 closeNav.addEventListener("click", () => {
   menu.classList.remove("show");
 });
+
+// fixed nav
+const navBar= document.querySelector(".nav")
+const navHeight = navBar.getBoundingClientRect().height;
+window.addEventListener('scroll',()=>{
+  const scrollHeight=window.pageYOffset;
+  if (scrollHeight>navHeight) {
+    navBar.classList.add('fix-nav')
+  }else{
+    navBar.classList.remove('fix-nav')
+  }
+})
